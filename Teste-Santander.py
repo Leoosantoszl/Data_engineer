947b1217237849376e43a247aec07f9ebf7ee1b8
Leonardo Oliveira <117474875+Leoosantoszl@users.noreply.github.com>
2024-06-25 22:06:37 -0300

    Teste-Santander.py

--- Teste-Santander.py
+++ Teste-Santander.py
@@ -59,3 +59,42 @@ conn.execute('''CREATE TABLE dados(
 df_final.to_sql('dados', conn, if_exists='replace', index=False)
 conn.commit()
 conn.close()
+
+
+#consultando os dados
+
+#OS 10 MAIORES Fonte_de_Recurso
+conn = sqlite3.connect('dados_santander')
+cursor = conn.cursor()
+consulta_sql = (''' SELECT *
+    FROM dados
+    ORDER BY Total DESC
+    LIMIT 10;''')
+cursor.execute(consulta_sql)
+
+resultados = cursor.fetchall()
+
+print("Os 10 maiores valores:")
+for resultado in resultados:
+    print(resultado[0])
+
+conn.close()
+
+
+
+#OS 10 MAIORES Despesas
+conn = sqlite3.connect('dados_santander')
+cursor = conn.cursor()
+consulta_sql = (''' SELECT Despesa
+    FROM dados
+    ORDER BY Total DESC
+    LIMIT 10;''')
+cursor.execute(consulta_sql)
+
+resultados = cursor.fetchall()
+
+print("Os 10 maiores valores:")
+for resultado in resultados:
+    print(resultado[0])
+
+conn.close()
